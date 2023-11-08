c52560870f0801693b7532a928ad3815bb2c1b2c
Yakup <yakupjoraev@gmail.com>
2023-11-08 13:03:06 +0300

    added map

--- src/js/main.js
+++ src/js/main.js
@@ -436,6 +436,46 @@ for (let anchor of anchors) {
   })
 };
 
+function map() {
+  const container = document.querySelector('.contacts')
+  if (!container) {
+    return null
+  }
+
+  let center = [54.180225, 45.177524];
+
+  function init() {
+
+
+    let map = new ymaps.Map('contacts-map', {
+      center: center,
+      zoom: 17
+    });
+
+    let placemark = new ymaps.Placemark(center, {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '../img/icons/pin.svg',
+      iconImageSize: [42, 42],
+      iconImageOffset: [-14, -64]
+    })
+
+    map.geoObjects.add(placemark);
+
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    // map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+  }
+
+  ymaps.ready(init);
+}
+
+map();
+
 
 const openModalBtns = document.querySelectorAll('.open-modal-btn');
 const closeModalBtns = document.querySelectorAll('.close-modal-btn');
